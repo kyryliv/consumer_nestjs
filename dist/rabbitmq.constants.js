1347e638d9c1a8ef19bd2812920764c38d4c3830
Oleg Kyryliv <kyryliv@gmail.com>
2026-05-23 23:08:47 +0300

    init start

--- dist/rabbitmq.constants.js
+++ dist/rabbitmq.constants.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RABBITMQ_EVENT = exports.RABBITMQ_QUEUE = exports.RABBITMQ_URL = exports.RABBITMQ_SERVICE = void 0;
 exports.RABBITMQ_SERVICE = 'RABBITMQ_SERVICE';
-exports.RABBITMQ_URL = process.env.RABBITMQ_URL ?? 'amqp://localhost:5672';
+exports.RABBITMQ_URL = process.env.RABBITMQ_URL ?? 'amqp://rabbitmq:passworD@localhost:5672';
 exports.RABBITMQ_QUEUE = process.env.RABBITMQ_QUEUE ?? 'main_queue';
 exports.RABBITMQ_EVENT = 'demo_event';
 //# sourceMappingURL=rabbitmq.constants.js.map
--- src/rabbitmq.constants.ts
+++ src/rabbitmq.constants.ts
@@ -1,4 +1,5 @@
 export const RABBITMQ_SERVICE = 'RABBITMQ_SERVICE';
-export const RABBITMQ_URL = process.env.RABBITMQ_URL ?? 'amqp://localhost:5672';
+export const RABBITMQ_URL =
+	process.env.RABBITMQ_URL ?? 'amqp://rabbitmq:passworD@localhost:5672';
 export const RABBITMQ_QUEUE = process.env.RABBITMQ_QUEUE ?? 'main_queue';
 export const RABBITMQ_EVENT = 'demo_event';
